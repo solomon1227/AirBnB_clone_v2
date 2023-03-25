1575bfd32c440e50bcb76e41afa3d7db7718e892
solomon1227 <solomonf1227@gmail.com>
2023-03-25 22:25:13 +0300

    update so that i can choose between filestorage and db storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
 
+import os
 
-storage = FileStorage()
-storage.reload()
+storage_type = getenv(HBNB_TYPE_STORAGE)
+
+if  storage_type == 'db': #import db storage
+    from models.engine.file_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else: #import filestorage
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
