f58e2af3b4f827064858910ac8ec54b607822d41
solomon1227 <solomonf1227@gmail.com>
2023-03-25 22:26:53 +0300

    Update state so that it can be possible to store the city instance in db storage

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if models.storage_type == "db"
+        __tablename__ = "cities"
+
+        name = Column(String(128), nullable=Flase)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=Flase)
+        state = relationship('State', back_populates='cities')
+    else:
+        name = ""
+        state_id = 
