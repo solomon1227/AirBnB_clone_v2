d1469f80346b01d3117890c49faeddce35fb0274
solomonferede <solomonf1227@gmail.com>
2023-04-22 21:52:24 +0300

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 import models
 
 
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 import models
 
 
@@ -13,6 +14,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place', back_populates='user')
     else:
         email = ''
         password = ''
