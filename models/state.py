222216994fcb27c4286a73f49cce2b1b9c7cfb75
solomonferede <solomonf1227@gmail.com>
2023-04-22 14:24:28 +0300

    add relationship between state and city

--- models/state.py
+++ models/state.py
@@ -2,7 +2,6 @@
 """ holds class State"""
 import models
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -11,19 +10,7 @@ class State(BaseModel, Base):
     """Representation of state """
     if models.storage_type == "db":
         __tablename__ = 'states'
-        id = Column(String(60), nullable=False, primary_key=True)
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
     else:
         name = ""
 
-    if models.storage_type != "db":
-        @property
-        def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
