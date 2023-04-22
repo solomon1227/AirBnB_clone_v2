f3d1cc0529ae62f82692be913f3a5be6364acb7e
solomonferede <solomonf1227@gmail.com>
2023-04-22 15:45:01 +0300

    user class mapper

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ import models
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if models.storage_type == "db":
-        tablename__ = "users"
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
