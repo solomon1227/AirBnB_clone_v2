ff1aba3d264edff1a3e7bffa13e9d02870f2f0f6
solomonferede <solomonf1227@gmail.com>
2023-04-22 14:32:13 +0300

    add relationship between state and city

--- models/city.py
+++ models/city.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.state import State
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 import models
 
 
@@ -14,21 +13,7 @@ class City(BaseModel, Base):
         __tablename__ = "cities"
 
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('State.states.id'), nullable=False)
-        state = relationship('State', back_populates='cities')
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     else:
         name = ""
         state_id = ""
-
-if models.storage_type == "db":
-    State.cities = relationship("City", order_by=City.id, back_populates='state')
-else:
-    @property
-    def cities(self):
-        """getter for list of city instances related to the state"""
-        city_list = []
-        all_cities = models.storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """ holds class State"""
 import models
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -11,6 +12,16 @@ class State(BaseModel, Base):
     if models.storage_type == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
+        cities = relationship("City", order_by=City.id, back_populates='state')
     else:
         name = ""
-
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+City.state = relationship('State', back_populates='cities')
