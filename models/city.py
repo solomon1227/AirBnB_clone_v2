1ad85337c9d41f377685e7c3e58c05f6a2b4ff2e
solomonferede <solomonf1227@gmail.com>
2023-07-04 12:03:06 +0300

    upadte db storage

--- models/city.py
+++ models/city.py
@@ -13,8 +13,7 @@ class City(BaseModel, Base):
 
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'))
-        state = relationship('State', back_populates='cities')
-        places = relationship('Place', back_populates='cities')
+        places = relationship('Place', backref="cities", cascade="delete")
     else:
         name = ""
         state_id = ""
