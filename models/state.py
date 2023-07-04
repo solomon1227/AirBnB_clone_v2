1eca6814d90ef4017fb9cabec072b0f39265d460
solomonferede <solomonf1227@gmail.com>
2023-07-04 12:03:20 +0300

    update to include cascade deletae

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     if models.storage_type == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", order_by=City.id, backref="state", cascade="all, delete")
+        cities = relationship("City", backref="state", cascade="delete")
 
     else:
         name = ""
