3225ddf486c3eb4febbe2345c6096d6374a6e968
solomonferede <solomonf1227@gmail.com>
2023-07-05 16:02:17 +0300

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install and configure nginx web server
+apt-get update
+apt-get install -y nginx
+
+#prepare the directory
+mkdir /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo "<h1>Test file<h1>
+<p>This is a simple test file</p>
+<h2>the test is sucessful</h2> " > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#change ownership to ubuntu user and group
+chown ubuntu /data/
+chgrp ubuntu /data/
+
+# Edit Nginx configuration file
+sed -i '/^server {/a location \/hbnb_static { alias \/data\/web_static\/current\/; }' /etc/nginx/sites-available/default
+
+# Restart Nginx
+service nginx restart
+
