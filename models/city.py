c38f19aadd32269ad43441107c033a510fa86670
solomonferede <solomonf1227@gmail.com>
2023-07-05 16:01:14 +0300

    DBStorage-city

--- models/city.py
+++ models/city.py
@@ -13,7 +13,7 @@ class City(BaseModel, Base):
 
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'))
-        #places = relationship('Place', backref="cities", cascade="delete")
+        places = relationship('Place', backref="cities", cascade="delete")
     else:
         name = ""
         state_id = ""
