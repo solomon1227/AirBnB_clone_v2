8ee4a8d099714dde7f98be5f9142cd805b0aef28
solomonferede1 <solomonf1227@gmail.com>
2023-07-06 20:36:46 +0000

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,22 +18,19 @@ env.key_filename = '~/.ssh/id_rsa'
 def do_deploy(archive_path):
     '''Deploys an archive to the web servers.'''
 
-    # Check if the archive file exists
     if not path.exists(archive_path):
         return False
-    
     try:
         put(archive_path, "/tmp/")
         filename = path.basename(archive_path)
         filename_without_extension = path.splitext(archive_filename)[0]
         release_folder = "/data/web_static/releases/{}".format(filename_without_extension)
         run("mkdir -p {}".format(release_folder))
-	
         run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
-
         run("rm /tmp/{}".format(archive_filename))
         run("rm -f /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(release_folder))
         return True
     except Exception as e:
         return False
+
