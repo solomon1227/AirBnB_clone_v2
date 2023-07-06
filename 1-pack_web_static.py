1ee192f06a1916052dfe7f90b5e8afdd03360290
solomonferede <solomonf1227@gmail.com>
2023-07-06 12:38:23 +0300

    pack the web static - archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,9 +7,9 @@ def do_pack():
     """Pack the web static folder in to .tgz file format"""
     try:
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_name = "web_static_" + current_time + ".tgz"
         local("mkdir -p versions")
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-        return "versions/{}".format(archive_name)
+        archive_name = "versions/web_static_" + current_time + ".tgz"
+        local("tar -cvzf {} web_static".format(archive_name))
+        return (archive_name)
     except Exception as e:
         return None
