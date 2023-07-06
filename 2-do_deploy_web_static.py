214b81a4410db171df2ba3ad15ed034230fc1efa
solomonferede1 <solomonf1227@gmail.com>
2023-07-06 19:16:10 +0000

    deploay static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+from fabric.api import env, run, put
+from os import path, environ
+
+
+# Set web server IP addresses
+env.hosts = ['54.237.25.186', '100.25.21.22']
+env.user = environ.get('USERNAME', 'ubuntu')
+env.key_filename = environ.get('SSH_KEY_PATH', '/.ssh/id_rsa')
+
+def do_deploy(archive_path):
+    '''Deploys an archive to the web servers.'''
+
+    # Check if the archive file exists
+    if not path.exists(archive_path):
+        return False
+    
+    try:
+        put(archive_path, "/tmp/")
+        filename = path.basename(archive_path)
+        filename_without_extension = path.splitext(archive_filename)[0]
+        release_folder = "/data/web_static/releases/{}".format(filename_without_extension)
+        run("mkdir -p {}".format(release_folder))
+	
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
+
+        run("rm /tmp/{}".format(archive_filename))
+        run("rm -f /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(release_folder))
+        print("New version deployed successfully!")
+        return True
+    except Exception as e:
+        print("Deployment failed: {}".format(e))
+        return False
