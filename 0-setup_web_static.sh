dd808a540f44182207ebbadb83eb317f02efc445
solomonferede <solomonf1227@gmail.com>
2023-07-06 10:32:53 +0300

    setting up static web

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,10 @@
 # setting up web server for static web
 
 apt-get -y update
-apt-get -y upgrade
 apt-get -y install nginx
-mkdir -p /data/web_static/releases/test /data/web_static/shared
-"This is a test" | tee /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "This is a test" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
