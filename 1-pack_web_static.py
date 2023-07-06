47865ca26aee942c6745de6e9a13361c5579626a
solomonferede <solomonf1227@gmail.com>
2023-07-06 12:25:01 +0300

    pack the web static - archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 from fabric.api import local
 from datetime import datetime
 
 def do_pack():
+    """Pack the web static folder in to .tgz file format"""
     try:
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
         archive_name = "web_static_" + current_time + ".tgz"
