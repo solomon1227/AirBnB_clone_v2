67e8d8faed5708e973117a1521d84efa20f3b5b5
ubuntu <ubuntu@ubuntu.ethiotech.tech>
2023-07-08 23:29:23 +0300

    deploy static web

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,12 +13,12 @@ env.hosts = ['54.237.25.186', '100.25.21.22']
 def do_clean(number=0):
     """Delete out-of-date archives"""
     if number == 0 or number == 1:
-        number = 2 # Keep only the most recent and second most recent versions
+        num = 2  # Keep only the most recent and second most recent versions
     else:
-        number += 1
+        num = number + 1
 
     with lcd("versions"):
-        local("ls -t | tail -n +{} | xargs -I {{}} rm -- {{}}".format(number))
+        local("ls -t | tail -n +{} | xargs -I {{}} rm -- {{}}".format(num))
 
     with cd("/data/web_static/releases"):
-        run("ls -t | tail -n +{} | xargs -I {{}} rm -rf -- {{}}".format(number))
+        run("ls -t | tail -n +{} | xargs -I {{}} rm -rf -- {{}}".format(num))
