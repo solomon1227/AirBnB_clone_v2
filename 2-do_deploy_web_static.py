7c2bb7f3d1c0dedb3be0255274094bab0e46a91a
solomonferede1 <solomonf1227@gmail.com>
2023-07-10 03:59:54 +0000

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,14 +19,15 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         filename = path.basename(archive_path)
         file_withoutext = path.splitext(filename)[0]
-        sudo("chown -R ubuntu:ubuntu /data/")
+        sudo("mkdir -p /data/")
+        sudo("chown -R $USER:$USER /data/")
         release_folder = "/data/web_static/releases/{}".format(file_withoutext)
-        run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{} -C {}".format(filename, release_folder))
-        run("rm /tmp/{}".format(filename))
-        run('mv -u {}/web_static/* {}'.format(release_folder, release_folder))
-        run("rm -f /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_folder))
+        sudo("mkdir -p {}".format(release_folder))
+        sudo("tar -xzf /tmp/{} -C {}".format(filename, release_folder))
+        sudo("rm /tmp/{}".format(filename))
+        sudo('mv -u {}/web_static/* {}'.format(release_folder, release_folder))
+        sudo("rm -f /data/web_static/current")
+        sudo("ln -s {} /data/web_static/current".format(release_folder))
         print("A new version deployed")
         return True
     except Exception as e:
