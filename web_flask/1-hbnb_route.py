156f02d72d591a0ab2c9a7d2aa23e744bdfd7bda
solomonferede <solomonf1227@gmail.com>
2023-07-22 07:45:08 +0300

    routing / and /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0-hello_route module - host a simple app on local host using flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """display Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return 'HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
