67435a639c4ccd4f13455f5a058cf99dee049f0e
solomonferede <solomonf1227@gmail.com>
2023-07-22 07:34:57 +0300

    A minimal flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+0-hello_route module - host a simple app on local host using flask
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,4 +9,5 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """minimal app defination"""
     return 'Hello HBNB!'
