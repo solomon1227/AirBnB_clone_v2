e153d6802f0f229d62a904581bd01335d1088656
solomonferede <solomonf1227@gmail.com>
2023-07-29 21:38:20 +0300

    list all states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@ Starts flask web application
 """
 
 from flask import Flask, render_template
-from models import storage
+from models import *
 
 
 app = Flask(__name__)
@@ -13,12 +13,12 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def list_state():
     """return an html templet that lists all states"""
-    states = storage.all('State').values()
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down():
+def tear_down(exception):
     """Close a session and start a new session"""
     storage.close()
 
