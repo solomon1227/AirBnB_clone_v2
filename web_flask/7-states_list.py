d54b99b860a3bf3576bfc645e2c68ac67dc0aeed
solomonferede <solomonf1227@gmail.com>
2023-07-29 20:52:38 +0300

    list all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Starts flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_state():
+    """return an html templet that lists all states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down():
+    """Close a session and start a new session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
