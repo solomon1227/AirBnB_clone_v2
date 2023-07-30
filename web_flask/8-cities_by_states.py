06e74ec05b829bebfa3ea05d04f8632d1bd90134
solomonferede <solomonf1227@gmail.com>
2023-07-30 11:42:15 +0300

    cities by state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starts flask web application
+"""
+
+from flask import Flask, render_template
+from models import *
+from models.state import State
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_state():
+    """return an html templet that lists all states"""
+    states = (storage.all("State").values())
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """Close a session and start a new session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
