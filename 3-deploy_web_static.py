cdab6a058c2fbe20379388f1883da76c10af2d7d
Solomon <solomonf1227@gmail.com>
2024-07-11 16:10:11 +0300

    Full deployment of static web for both serevers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -16,6 +16,7 @@ def do_pack():
     try:
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
+        local("chown -hR $USER:$USER versions")
         archive_name = "versions/web_static_" + current_time + ".tgz"
         local("tar -cvzf {} web_static".format(archive_name))
         return (archive_name)
