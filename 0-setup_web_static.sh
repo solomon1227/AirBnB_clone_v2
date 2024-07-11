35f4ff26c651a01583291238178554e03c6f9fa2
Solomon <solomonf1227@gmail.com>
2024-07-11 10:30:46 +0300

    Setting up the serever for static content release

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
 # setting up web server for static web
+
+#upgrate and install nginx
 apt-get upgarde
 apt-get -y update
 apt-get -y install nginx
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo "This is a web_static test html file" > /data/web_static/releases/test/index.html
+
+# Create folders
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+
+#Fake html test page
+echo "<html>
+<head>
+  <title>Test Page</title>
+</head>
+<body>
+  <h1>Welcome to the Test Page</h1>
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+#Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#Change ownerships
 chown -hR ubuntu:ubuntu /data/
-sed -i '/^server {/a\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-service nginx start
+
+#Update the serever to serve /hbnb_static/
+sed -i '/server_name/a \
+    \    location /hbnb_static/ {\
+    \        alias /data/web_static/current/;\
+    \        autoindex on;\
+    \    }' /etc/nginx/sites-available/default
+
+service nginx restart
