00a0ea9bdcdba5b0508107fd545ba56d2ed227fc
Solomon <solomonf1227@gmail.com>
2024-07-12 10:08:41 +0300

    Delete out of date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,64 +4,24 @@ script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
 """
 
-from fabric.api import env, local, put, run, lcd, cd
+from fabric.api import env, local, run, lcd, cd
 from datetime import datetime
-from os import path
+from os import listdir
 env.hosts = ['54.237.25.186', '100.25.21.22']
 
-
-def do_pack():
-    """Pack the web static folder in to .tgz file format"""
-    try:
-        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
-        archive_name = "versions/web_static_" + current_time + ".tgz"
-        local("tar -cvzf {} web_static".format(archive_name))
-        return (archive_name)
-    except Exception as e:
-        return None
-
-
-def do_deploy(archive_path):
-    '''Deploys an archive to the web servers.'''
-
-    if not path.exists(archive_path):
-        return False
-    try:
-        put(archive_path, "/tmp/")
-        filename = path.basename(archive_path)
-        file_withoutext = path.splitext(filename)[0]
-        release_folder = "/data/web_static/releases/{}".format(file_withoutext)
-        run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{} -C {}".format(filename, release_folder))
-        run("rm /tmp/{}".format(filename))
-        run('mv -u {}/web_static/* {}'.format(release_folder, release_folder))
-        run("rm -f /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_folder))
-        print("A new version deployed")
-        return True
-    except Exception as e:
-        print("Failed: {}".format(e))
-        return False
-
-
-def deploy():
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
 
 
 def do_clean(number=0):
     """Delete out-of-date archives"""
-    if number == 0 or number == 1:
-        num = 2  # Keep only the most recent and second most recent versions
-    else:
-        num = int(number) + 1
+    number = 1 if int(number) == 0 else int(number)
 
+    archives = sorted(listdir("versions"))
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
-        local("ls -t | tail -n +{} | xargs -I {{}} rm -- {{}}".format(num))
+        [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-        run("ls -t | tail -n +{} | xargs -I {{}} rm -rf -- {{}}".format(num))
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
