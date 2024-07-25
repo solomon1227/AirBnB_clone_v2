51780f64b08be2f5968276189190eea8f73f81a7
Solomon <solomonf1227@gmail.com>
2024-07-25 14:27:46 +0300

    Adding route from 0- i.e HBNB"

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-0-hello_route module - host a simple app on local host using flask
-"""
+'''Start flask web application'''
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,15 +8,16 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """display Hello HBNB"""
+    '''Start minimal Flask web application:'''
+
     return 'Hello HBNB!'
 
-
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """Display HBNB"""
+    '''Routing function for /hbnb'''
+
     return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
