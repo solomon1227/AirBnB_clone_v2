d3ae839621ab50c7532d484c072ffb10e3735b28
Solomon <solomonf1227@gmail.com>
2024-07-25 13:58:52 +0300

    Create first flask web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-0-hello_route module - host a simple app on local host using flask
-"""
+'''Start flask web application'''
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello():
-    """minimal app defination"""
+    '''Defination of a function returning Hello HBNB!'''
+
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='500', debug=True)
