61bc2d400efb2a12ace15680bb3325822216f89c
Solomon <solomonf1227@gmail.com>
2024-07-25 14:17:39 +0300

    Flask Minimal web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    '''Defination of a function returning Hello HBNB!'''
+    '''Definition of a function returning Hello HBNB!'''
 
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='500', debug=True)
+    app.run(host='0.0.0.0', port=5000)
