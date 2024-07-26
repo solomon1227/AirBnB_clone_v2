8ec0193483a3c86e35e3546f553ca7f8d173f366
Solomon <solomonf1227@gmail.com>
2024-07-26 15:04:33 +0300

    States list - sorted

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-Starts flask web application
-"""
+'''Start flask web application for AirBNB clone'''
 
 from flask import Flask, render_template
 from models import *
 from models import storage
+
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def list_state():
-    """return an html templet that lists all states"""
+def states_list():
+    '''Start minimal Flask web application:'''
+
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """Close a session and start a new session"""
+def remove_session(exception):
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
