8bc506f4a01ebd6357b61394f54df2cd90d83ea9
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-07-27 08:16:35 +0300

    List states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-Starts flask web application
-"""
+'''Start flask web application for AirBNB clone'''
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def list_state():
-    """return an html templet that lists all states"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+def states_list():
+    '''Start minimal Flask web application:'''
+
+    states = storage.all("State").values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """Close a session and start a new session"""
+def remove_session(exception=None):
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
