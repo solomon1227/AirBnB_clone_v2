e26c6e78ecff6622479e9f66f525d69e30adac30
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-07-27 09:58:50 +0300

    cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-'''Start flask web application for AirBNB clone'''
-
+"""
+    Sript that starts a Flask web application
+"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
-
+import os
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    '''Start minimal Flask web application that lists cities by state'''
-
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+def city_state_list():
+    """
+        method to render states from storage
+    """
+    states = storage.all('State').values()
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def remove_session(exception=None):
+def handle_teardown(self):
+    """
+        method to handle teardown
+    """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
