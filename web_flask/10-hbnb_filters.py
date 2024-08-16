84a9fd7d730a859428e16d69138cdbbd45cf08c3
solomonferede1 <solomonf1227@gmail.com>
2024-08-16 06:26:14 +0300

    Filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -8,23 +8,18 @@ import os
 app = Flask(__name__)
 
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """Method to filter places and states"""
+
+
+@app.teardown_appcontext
 def handle_teardown(self):
     """
         method to handle teardown
     """
     storage.close()
 
-
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters_list():
-    """
-        method to display html page 6-index.html
-    """
-    states = storage.all('State').values()
-    amenities = storage.all('Amenity').values()
-    return render_template(
-        "10-hbnb_filters.html",
-        states=states, amenities=amenities)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
